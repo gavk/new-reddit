4f230c4e5b231b447a03e3ee2e8f630b7ca2d84e
gavk <gavk@ngs.ru>
2021-10-10 11:12:23 +1000

    fix: The 'mode' option has not been set, webpack will fallback to 'production' for this value

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
+const NODE_ENV = process.env.NODE_ENV;
+
 module.exports = {
 	entry: path.resolve(__dirname, 'src/index.jsx'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'index.js',
 	},
+	mode: NODE_ENV ? NODE_ENV : "development",
 };
