5894fb1f1eba4efb62e1e1ae685d35e64efdb14f
gavk <gavk@ngs.ru>
2021-10-10 12:07:02 +1000

    Сделал скрипт, запускающий dev-сервер, для облегчения разработки

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV === 'development';
 
 module.exports = {
 	entry: path.resolve(__dirname, 'src/index.jsx'),
@@ -26,4 +27,9 @@ module.exports = {
 			template: path.resolve(__dirname, 'index.html'),
 		})
 	],
+	devServer: {
+		port: 3000,
+		open: true,
+		hot: IS_DEV,
+	},
 };
